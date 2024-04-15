95daebda626d00bd715f48834d4aa2f2432617ab
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-04-15 11:05:20 -0700

    Just send back text string 23

--- llava/serve/gradio_web_server.py
+++ llava/serve/gradio_web_server.py
@@ -607,11 +607,13 @@ def build_demo(concurrency_count=10):
 
             t0 = time.time()
             res = ""
+            assert not include_image1, "include_image must be False"
             for ret in http_bot(state1, model_selector1, temperature1, top_p1, max_output_tokens1, include_image1,
                                       request):
                 print(ret)
-                if ret and len(ret[-1]) == 2 and ret[-1][-1]:
-                    res = ret[-1][-1]
+                ret1 = ret[1]
+                if ret1 and len(ret1[-1]) == 2 and ret1[-1][-1]:
+                    res = ret1[-1][-1]
                     yield res
             print("Duration http_bot: %s" % (time.time() - t0), flush=True)
 
